fc668dd0bcd347a068c68a900adc6764f0b93987
Rajesh kumar <rajesh.kumar@goldenscent.com>
2020-12-22 19:19:41 +0400

    View Layer

--- app/code/Mastering/SampleModule/Controller/Index/Index.php
+++ app/code/Mastering/SampleModule/Controller/Index/Index.php
@@ -1,20 +1,13 @@
 <?php
-namespace Mastering\SampleModule\Controller\Index;
 
+namespace Mastering\SampleModule\Controller\Index;
 
 use Magento\Framework\Controller\ResultFactory;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
-
-    public function execute() {
-
-        /** @var \Magento\Framework\Controller\Result\Raw $result */
-
-        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
-        $result->setContents("Hello world");
-        return $result;
+    public function execute()
+    {
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
-
 }
-
